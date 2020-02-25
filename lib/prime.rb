bdd2111259041b7698b47d5bb4c4b1d6a3374464
Charles Pelton <charliejp0311@gmail.com>
2020-02-25 01:49:06 +0000

    Automatically backed up by Learn

---
+++ lib/prime.rb
@@ -0,0 +1,17 @@
+def prime_euler
+  num1 = 3
+  num2 = 5
+  i = 0
+  nums = []
+  while i < 1000
+    if i % 3 == 0 || i % 5 == 0
+      nums << i 
+    end
+  end
+  sum_of_all = 0
+  nums.each do |num|
+    sum_of_all += num
+  end
+  puts nums
+  puts sum_of_all
+end
